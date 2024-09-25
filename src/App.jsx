75ac700a4af39697bdd1624e64a145fdff1e4206
Armando Ruiz <armando.ruiz@industrie.co>
2024-09-25 15:16:57 +1000

    agregado los productos y ensenandolos en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { Products } from './components/Products'
+import { db } from './mocks/db'
 function App() {
+
+  const [products, setProducts] = useState(db)
 
   return (
     <>
       <Header />
-      <Products />
+      <Products products={products} />
       <Footer/>
     </>
   )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,20 @@
-import { Product } from './components/Product'
+import {Product} from "./Product";
 
 
-export function Products() {
+
+export function Products({ products }) {
 
   return(
     <main className="container-xl mt-5">
       <h2 className="text-center">Nuestra Colección</h2>
 
       <div className="row mt-5">
-        <Product />
+        {products.map( product => (
+          <Product
+            key={product.id}
+            product={product}
+            />
+        ))}
       </div>
     </main>
   )
